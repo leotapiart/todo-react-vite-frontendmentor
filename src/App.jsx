214b1aad369da8718458b6a794ab2b8079509c7e
leotapiart2 <leonel.tapia.rios@gmail.com>
2022-11-03 17:05:38 -0300

    Cambios en el trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const initialStateTodos = [
   { id: 2, title: "10 minutes meditation", completed: false },
   { id: 3, title: "Read for 1 hour", completed: false },
   { id: 4, title: "Pick up groceries", completed: false },
-  { id: 5, title: "Complete Todo App on Frontend Mentor", completed: false },
+  { id: 5, title: "Complete Todo App on Frontend Mentor Cenco", completed: false },
 ];
 
 function App() {
